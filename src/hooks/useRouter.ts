9ecc89545a4aaeae330828207a75ce8df824d0ec
zyi <1182900548@qq.com>
2024-05-17 23:03:41 +0800

    fix

--- src/hooks/useRouter.ts
+++ src/hooks/useRouter.ts
@@ -1,5 +1,3 @@
 import {IFY} from "../types";
-import {Dispatcher} from "undici-types";
-import HttpMethod = Dispatcher.HttpMethod;
 
-export const useRouter = (method: HttpMethod, path: string, handler: IFY.Handler) => ({method, path, handler})
+export const useRouter = (method: IFY.HttpMethod, path: string, handler: IFY.Handler) => ({method, path, handler})
